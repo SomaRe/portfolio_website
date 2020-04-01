aca1e94ad10faa96a8f9dee10069c4fd04aebdc6
somaRe <shivatej82@gmail.com>
2020-04-01 15:12:39 +0530

    media queries introduced

---
+++ script.js
@@ -0,0 +1,22 @@
+console.log("Hello")
+
+/*function info1(){
+  /  console.log("working")
+    var x = document.getElementsByClassName("info1-v");
+    var y = document.getElementsByClassName('info1-inv')[0];
+    var inn = document.getElementsByClassName('inner')[0];
+    var info1 = document.getElementsByClassName("info1")[0];
+    for (var i=0;i<x.length;i+=1){
+        if (x[i].style.display === "none") {
+            x[i].style.display = "block";
+            y.style.display = "none";
+            inn.style.backgroundColor = "#0b0c10";
+            info1.innerHTML='i';
+          } else {
+            x[i].style.display = "none";
+            y.style.display = "block";
+            inn.style.backgroundColor = "#45A29E";
+            info1.innerHTML='X';
+          } 
+      }
+}*/
